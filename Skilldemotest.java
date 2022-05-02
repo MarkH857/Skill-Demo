ffd535b59322bae201902d0ccb0f2cdfaa10804b
MarkH857 <97138169+MarkH857@users.noreply.github.com>
2022-05-01 23:55:37 -0700

    Add a test

---
+++ Skilldemotest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class Skilldemotest {
+    @Test
+    public void testMultiply() {
+        assertEquals(2, Skilldemo.multiply(2,3));
+    }
+
+}
